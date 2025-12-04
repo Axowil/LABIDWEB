7ec2524b6bfbafc250630ff1567b2308ab3cd4ec
Willian Apaza <169059656+Axowil@users.noreply.github.com>
2025-12-04 15:35:06 -0500

    Add files via upload

--- LAB-IDWEB19/ejercicios.js
+++ LAB-IDWEB19/ejercicios.js
@@ -1,31 +1,17 @@
-const ejercicio1 = document.getElementById("ejercicio1");
-
-ejercicio1.addEventListener("click", async () => { 
-    let id;
-    id = prompt(`Ingrese el id de pokemon`);
+const ejercicio4 = document.getElementById("ejercicio4");
+ejercicio4.addEventListener("click", async() => {
     try {
-        const pokemonData = await cargarPokemonNombre(id);
-        console.log("¡Datos del Pokémon obtenidos!");
-        console.log(pokemonData);
-        console.log(`Nombre: ${pokemonData.name}`); 
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon/charizard");
+
+        if (!res.ok) {
+            throw new Error("Error en la solicitud: " + res);
+        }
+
+        const data = await res.json();
+
+        console.log("URL imagen: " + data.sprites.front_default);
 
     } catch (error) {
-        console.error("Ocurrió un error al buscar el Pokémon:", error.message);
+        console.log(error);
     }
 });
-
-async function cargarPokemonNombre(id) {
-    if (!id) {
-        throw new Error("No se ingresó un ID de Pokémon.");
-    }
-    try {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        if (!res.ok) {
-            throw new Error(`Error en la solicitud: ${res.status} (${res.statusText})`);
-        }
-        const data = await res.json();
-        return data;
-    } catch (error) {
-        throw error; 
-    }
-}
